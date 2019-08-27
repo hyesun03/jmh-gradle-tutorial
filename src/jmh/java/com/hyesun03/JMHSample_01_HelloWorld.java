e5fbf121baca4c39e2fdd6af03514cce559c6a02
hyesun03 <chsun0303@gmail.com>
2019-08-27 19:14:44 +0900

    Add JMHSample_01_HelloWorld.java

---
+++ src/jmh/java/com/hyesun03/JMHSample_01_HelloWorld.java
@@ -0,0 +1,24 @@
+package com.hyesun03;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class JMHSample_01_HelloWorld {
+
+    @Benchmark
+    public void wellHelloThere() {
+        // this method was intentionally left blank.
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_01_HelloWorld.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
